cd94a383830db7eaf1c66501b3bc7f8c6e4ae0c0
Minjie Wan （万敏杰） <38133231+MinjieWan@users.noreply.github.com>
2026-05-07 16:52:44 +0800

    Add files via upload

---
+++ ptflops/flops_counter.py
@@ -0,0 +1,46 @@
+import sys
+
+import torch.nn as nn
+
+from .pytorch_engine import get_flops_pytorch
+from .utils import flops_to_string, params_to_string
+
+
+def get_model_complexity_info(model, input_res,
+                              print_per_layer_stat=True,
+                              as_strings=True,
+                              input_constructor=None, ost=sys.stdout,
+                              verbose=False, ignore_modules=[],
+                              custom_modules_hooks={}, backend='pytorch',
+                              flops_units=None, param_units=None,
+                              output_precision=2):
+    # assert type(input_res) is (tuple or list)
+    assert len(input_res) >= 1
+    assert isinstance(model, nn.Module)
+
+    if backend == 'pytorch':
+        flops_count, params_count = get_flops_pytorch(model, input_res,
+                                                      print_per_layer_stat,
+                                                      input_constructor, ost,
+                                                      verbose, ignore_modules,
+                                                      custom_modules_hooks,
+                                                      output_precision=output_precision,
+                                                      flops_units=flops_units,
+                                                      param_units=param_units)
+    else:
+        raise ValueError('Wrong backend name')
+
+    if as_strings:
+        flops_string = flops_to_string(
+            flops_count,
+            units=flops_units,
+            precision=output_precision
+        )
+        params_string = params_to_string(
+            params_count,
+            units=param_units,
+            precision=output_precision
+        )
+        return flops_string, params_string
+
+    return flops_count, params_count
---
+++ ptflops/utils.py
@@ -0,0 +1,36 @@
+def flops_to_string(flops, units=None, precision=2):
+    if units is None:
+        if flops // 10**9 > 0:
+            return str(round(flops / 10.**9, precision)) + ' GMac'
+        elif flops // 10**6 > 0:
+            return str(round(flops / 10.**6, precision)) + ' MMac'
+        elif flops // 10**3 > 0:
+            return str(round(flops / 10.**3, precision)) + ' KMac'
+        else:
+            return str(flops) + ' Mac'
+    else:
+        if units == 'GMac':
+            return str(round(flops / 10.**9, precision)) + ' ' + units
+        elif units == 'MMac':
+            return str(round(flops / 10.**6, precision)) + ' ' + units
+        elif units == 'KMac':
+            return str(round(flops / 10.**3, precision)) + ' ' + units
+        else:
+            return str(flops) + ' Mac'
+
+
+def params_to_string(params_num, units=None, precision=2):
+    if units is None:
+        if params_num // 10 ** 6 > 0:
+            return str(round(params_num / 10 ** 6, precision)) + ' M'
+        elif params_num // 10 ** 3:
+            return str(round(params_num / 10 ** 3, precision)) + ' k'
+        else:
+            return str(params_num)
+    else:
+        if units == 'M':
+            return str(round(params_num / 10.**6, precision)) + ' ' + units
+        elif units == 'K':
+            return str(round(params_num / 10.**3, precision)) + ' ' + units
+        else:
+            return str(params_num)
